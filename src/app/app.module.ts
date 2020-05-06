3c1f385fc650039460d5ece92ed14c7516ba3c4a
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-06 12:25:09 +0300

    add diary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiaryComponent } from './diary/diary.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    DiaryComponent,
     
   ],
   imports: [
