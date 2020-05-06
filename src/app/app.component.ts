f14f9f8e287c3348aacc98b1c9f37ec0ace92d03
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-06 12:09:58 +0300

    add id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Diary } from "./diary";
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  diaries:string[];
-
-  constructor(){
-    this.diaries = ['Dear Diary, Today marks the beginning of my life', 'Dear Diary, I am super sleepy although I have to study', 'Dear Diary, Angular sucks!!']
-  }
+  diaries:Diary[] = [
+{id:1, name:'Dear Diary, Today marks the beginning of my life'},
+{id:2, name:'Dear Diary, I am super sleepy although I have to study'},
+{id:3, name:'Dear Diary, Angular sucks!!'},
+  ];
   
 }
---
+++ src/app/diary.ts
@@ -0,0 +1,4 @@
+export class Diary {
+    id: number;
+    name: string;
+}
