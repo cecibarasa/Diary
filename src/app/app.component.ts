5d4a21cf7a18d41690bdb78761628f2bc1084848
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-06 11:55:00 +0300

    add arrays and constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Diary';
+  diaries:string[];
+
+  constructor(){
+    this.diaries = ['Dear Diary, Today marks the beginning of my life', 'Dear Diary, I am super sleepy although I have to study', 'Dear Diary, Angular sucks!!']
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DiaryFormComponent } from './diary-form/diary-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiaryFormComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
